ef41b611c14302d139d855500274409b8f83a391
SandeepaniGunasekara <115693602+SandeepaniGunasekara@users.noreply.github.com>
2023-12-03 10:27:58 +0530

    created frontend and backend folder structure

---
+++ Backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+app.use(cors())
+
+const db = mysql.createConnection({
+    host : "localhost",
+    user : 'root',
+    password:'ssss',
+    database : 'hobby'
+})
+
+app.get('/',(re,res)=>{
+    return res.json("from backend side");
+})
+
+app.get('/users',(req,res)=>{
+    const sql="SELECT * FROM users"
+    db.query(sql,(err,data)=>{
+        if (err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.listen(8081, ()=>{
+    console.log("listening");
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useEffect } from 'react'
+
+function App() {
+  useEffect(()=> {
+    fetch ('')
+
+  },[])
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
